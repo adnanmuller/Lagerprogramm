89a367ae1bb3378a94dd9d4b9c4ffc37894304bc
adnan_Casino <amueller@grandcasinobasel.com>
2021-12-14 18:56:19 +0100

    start14.12.21

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Techniker Lagerprogramm</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="CSS/styles.css">
+<style>
+</style>
+<script src=""></script>
+<body>
+  <h1 id="index_H1">Techniker Lagerprogramm</h1>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+<?php
+echo "Hello world";
+
+
+
+
+
+
+
+
+?>
